fb30d7adc33855c65dbc0be83e658b80e24bcd21
Kaustav Sarkar <93545845+kaustavsarkar14@users.noreply.github.com>
2023-09-10 11:01:37 +0530

    created a submission

--- script.js
+++ script.js
@@ -2,7 +2,24 @@ function mincost(arr)
 { 
 //write your code here
 // return the min cost
-  
+	 if (!arr || arr.length === 0) {
+    return 0;
+  }
+  let total = 0
+  while(arr.length>1){
+    const min1 = Math.min(...arr)
+    const min1_ind = arr.indexOf(min1)
+    arr.splice(min1_ind,1)
+
+    const min2 = Math.min(...arr)
+    const min2_ind = arr.indexOf(min2)
+    arr.splice(min2_ind,1)
+
+    total = total + min1 + min2
+    arr.push(min1+min2)
+
+  } 
+  return total
 }
 
 module.exports=mincost;
